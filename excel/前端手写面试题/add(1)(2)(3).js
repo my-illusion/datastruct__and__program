d2c3799aa343a327304c4a03bfd99c2835819011
宋冰冰 <wb.songbingbing@mesg.corp.netease.com>
2021-05-20 11:08:53 +0800

    add

---
+++ excel/前端手写面试题/add(1)(2)(3).js
@@ -0,0 +1,33 @@
+// 考察函数柯里化的写法
+
+// function curry(fn) {
+//     return function temp () {
+//         return arguments.length < fn.length ? (...args) => temp(...arguments, ...args) : fn(...arguments)
+//     }
+// }
+
+
+
+
+// function add(a, b, c) {
+//     return a + b + c
+// }
+
+// const curryAdd = curry(add)
+
+// console.log(curryAdd(1)(2)(3))
+
+// 实现 add(1)(2)(3)()
+function curry(fn) {
+    var args = []
+    return function temp() {
+        return !arguments.length ? fn(...args) : (args.push(...arguments), temp)
+    }
+}
+
+function add(...args) {
+    return args.reduce((a, b) => a + b)
+}
+
+const curryAdd = curry(add)
+console.log(curryAdd(1)(2)(3)(4)())
