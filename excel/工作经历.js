2012dcc002dbe0a62d389729295ac31df32b50b4
宋冰冰 <wb.songbingbing@mesg.corp.netease.com>
2021-06-18 14:22:07 +0800

    feat deepEqual

--- excel/工作经历.js
+++ excel/工作经历.js
@@ -9,7 +9,6 @@
     1、基于rollup抽离公共下拉复选等控件、解决@babel/runtime代码内联、组件内图片加载、less文件提取失败等问题
     https://github.com/my-illusion/MultiSelect.git(main branch)
     2、基于axios拦截器封装连续请求拦截工具，过滤连续请求且处于pending状态的数据请求
-    https://github.com/my-illusion/smart_axios.git
     3、实现echarts markLine自定义toolTip功能
     4、基于div的content-editable属性实现可编辑文案编排功能
     https://codepen.io/my-illusion/pen/BaWjvXp
@@ -21,14 +20,8 @@
 由于系统涉及的资源和用户数量较多，页面长列表等操作会有卡顿现象，因此有如下优化
 
     1、精细化UI组件颗粒度，添加组件离屏渲染逻辑加快页面渲染
-    https://codepen.io/my-illusion/pen/PopNpOo
     2、为了加速首屏渲染速度，开发滚动加载组件避免初始加载时不在视窗内的组件的渲染
-    https://codepen.io/my-illusion/pen/vYxXjzz?editors=1011
     3、基于antd-design的tree以及react-virtualized实现虚拟滚动，解决组件交互卡顿问题
-<<<<<<< HEAD
-    4、基于webpack-bundle-analyzer以及lighthouse评测工具做系统体验优化
-=======
     4、基于webpack-bundle-analyzer以及lighthouse做项目体验优化
->>>>>>> 16de670674a05e3466011deb711927b759e83720
     
 
