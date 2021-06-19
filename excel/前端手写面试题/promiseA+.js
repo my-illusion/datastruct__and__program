48d08391b8327df8db3e755ee739efa0b3097b43
songbb <2521728972@qq.com>
2021-06-19 11:19:45 +0800

    feat: 三数之和

---
+++ excel/前端手写面试题/promiseA+.js
@@ -0,0 +1,110 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+function Promise(excutor) {
+    let that = this
+    this.value = undefined
+    this.reason = undefined
+    this.status = PENDING
+    this.onFulfilledCallbacks = []
+    this.onRejectedCallbacks = []
+
+    function resolve(value) {
+        if(value instanceof Promise) {
+            return value.then(resolve, reject)
+        }
+
+        setTimeout(() => {
+            if(that.status === PENDING) {
+                that.status = FULFILLED
+                that.value = value
+                that.onFulfilledCallbacks.forEach(cb => cb(that.value))
+            }
+        }, 0)
+    }
+
+    function reject(reason) {
+        setTimeout(() => {
+            if (that.status === PENDING) {
+                that.status = REJECTED;
+                that.reason = reason;
+                that.onRejectedCallbacks.forEach(cb => cb(that.reason));
+            }
+        });
+    }
+
+    try{
+        excutor(resolve, reject)
+    }catch(err) {
+        reject(err)
+    }
+}
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    const that = this
+
+    let newPromise
+
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value
+
+    onRejected = typeof onRejected === 'function' ? onRejected : err => {
+        throw err
+    } 
+
+    if(that.status === FULFILLED) {
+        return newPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                try{
+                    let x = onFulfilled(that.value)
+                    resolve(x)
+                }catch(err) {
+                    reject(err)
+                }
+            })
+        })
+    }
+
+    if(that.status === REJECTED) {
+        return newPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                try{
+                    let x = onRejected(that.reason)
+                    resolve(x)
+                }catch(err) {
+                    reject(err)
+                }
+            })
+        })
+    }
+
+    if(that.status === PENDING) {
+        return newPromise = new Promise((resolve, reject) => {
+            that.onFulfilledCallbacks.push((value) => {
+                try {
+                    let x = onFulfilled(value);
+                    resolve(x)
+                } catch(e) {
+                    reject(e);
+                }
+            });
+            that.onRejectedCallbacks.push((reason) => {
+                try {
+                    let x = onRejected(reason);
+                    resolve(x)
+                } catch(e) {
+                    reject(e);
+                }
+            });
+        })
+    }
+}
+
+new Promise((resolve) => resolve(1))
+    .then(r1 => {
+        console.log(r1)
+        return 2
+    })
+    .then(r2 => {
+        console.log(r2)
+    })
---
+++ excel/双指针/三数之和.js
@@ -0,0 +1,48 @@
+// 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+// 使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+// 注意：答案中不可以包含重复的三元组。
+
+// 示例 1：
+
+// 输入：nums = [-1,0,1,2,-1,-4]
+// 输出：[[-1,-1,2],[-1,0,1]]
+// 示例 2：
+
+// 输入：nums = []
+// 输出：[]
+// 示例 3：
+
+// 输入：nums = [0]
+// 输出：[]
+
+// left  middle right
+function threeSum(nums) {
+    const result = []
+
+    nums.sort((a, b) => a - b)
+
+    for(let i = 0; i < nums.length - 2; i++) {
+        if(nums[i] > 0) return result
+
+        if(i > 0 && nums[i] === nums[i-1]) continue
+
+        let left = i + 1
+        let right = nums.length - 1
+        
+        while(right > left) {
+            if(nums[left] + nums[i] + nums[right] > 0) right--
+            else if(nums[left] + nums[i] + nums[right] < 0) left++
+            else {
+                result.push(
+                    [nums[i], nums[left], nums[right]]
+                )
+
+                while( right > left && nums[right] === nums[--right] );
+                while( right > left && nums[left] === nums[++left]);
+            }
+        }
+    }
+    return result
+}
+
