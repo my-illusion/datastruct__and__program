0f383d01e3b4ecb3c9642edfdc8804b7efb3a96c
宋冰冰 <wb.songbingbing@mesg.corp.netease.com>
2021-07-20 17:26:58 +0800

    add

--- excel/前端手写面试题/add(1)(2)(3).js
+++ excel/前端手写面试题/add(1)(2)(3).js
@@ -18,16 +18,32 @@
 // console.log(curryAdd(1)(2)(3))
 
 // 实现 add(1)(2)(3)()
-function curry(fn) {
-    var args = []
-    return function temp() {
-        return !arguments.length ? fn(...args) : (args.push(...arguments), temp)
+// function curry(fn) {
+//     var args = []
+//     return function temp() {
+//         return !arguments.length ? fn(...args) : (args.push(...arguments), temp)
+//     }
+// }
+
+// function add(...args) {
+//     return args.reduce((a, b) => a + b)
+// }
+
+// const curryAdd = curry(add)
+// console.log(curryAdd(1)(2)(3)(4)())
+
+
+function add(a) {
+    var temp = function(b) {
+        a += b
+        return temp
     }
+    temp.toString = function() {
+        return a
+    }
+    return temp
 }
 
-function add(...args) {
-    return args.reduce((a, b) => a + b)
-}
-
-const curryAdd = curry(add)
-console.log(curryAdd(1)(2)(3)(4)())
+console.log(
+    add(1)(2)(3) + 1
+ )
--- excel/前端手写面试题/event-bus.js
+++ excel/前端手写面试题/event-bus.js
@@ -1,47 +1,95 @@
+// class EventEmitter {
+//     constructor(){
+//         this.events = new Map()
+//     }
+
+//     addEventListener(event, fn){
+//         if(this.events.has(event)){
+//             const eventQueue = this.events.get(event)
+//             this.events.set(event, [...eventQueue, fn])
+//         }else{
+//             this.events.set(event, [fn])
+//         }
+//     }
+
+//     removeEventListener(event, fn){
+//         const queue = this.events.get(event)
+
+//         if(!queue) return
+
+//         if(!fn) {
+//             // clear all
+//             this.events.set(event, [])
+//         }else{
+//             this.events.set(event, queue.filter(f => f !== fn && f.callback !== fn))
+//         }
+//     }
+
+//     emit(event, ...args) {
+//         const queue = this.events.get(event)
+//         if(!queue) return
+
+//         queue.forEach(fn => {
+//             fn(...args)
+//         })
+//     }
+
+//     once(event, fn) {
+//         const that = this
+//         function wrapper(...args) {
+//             fn(...args)
+//             that.removeEventListener(event, wrapper)
+//         }
+
+//         wrapper.callback = fn
+
+//         this.addEventListener(event, wrapper)
+//     }
+// }
+
 class EventEmitter {
-    constructor(){
+    constructor(options){
+        this.options = options
+
         this.events = new Map()
     }
 
-    addEventListener(event, fn){
-        if(this.events.has(event)){
-            const eventQueue = this.events.get(event)
-            this.events.set(event, [...eventQueue, fn])
+    addEventListener(event, fn) {
+        const queue = this.events.get(event)
+        if(queue) {
+            this.events.set(event, [...queue, fn])
         }else{
             this.events.set(event, [fn])
         }
     }
 
-    removeEventListener(event, fn){
+    removeEventListener(event, fn) {
         const queue = this.events.get(event)
-
         if(!queue) return
 
         if(!fn) {
-            // clear all
             this.events.set(event, [])
-        }else{
-            this.events.set(event, queue.filter(f => f !== fn))
+            return
         }
+
+        this.events.set(event, queue.filter(cb => cb !== fn && cb.callback !== fn))
+    }
+
+    once(event, fn) {
+        const that = this
+        function wrappers(...args) {
+            fn(...args)
+            that.removeEventListener(event, wrappers)
+        }
+
+        wrappers.callback = fn
+        this.addEventListener(event, wrappers)
     }
 
     emit(event, ...args) {
         const queue = this.events.get(event)
         if(!queue) return
-
-        queue.forEach(fn => {
-            fn(...args)
-        })
-    }
-
-    once(event, fn) {
-        const that = this
-        function wrapper(...args) {
-            fn(...args)
-            that.removeEventListener(event, wrapper)
-        }
-
-        this.addEventListener(event, wrapper)
+        queue.forEach(fn => fn(...args))
     }
 }
 
@@ -53,14 +101,20 @@ function test(){
 
 emitter.addEventListener('click', test)
 
-emitter.removeEventListener('click', test)
+// emitter.removeEventListener('click', test)
 
 emitter.emit('click')
 
-emitter.once('move', () => { console.log('once') })
+function handleOnce() {
+    console.log('once')
+}
+
+emitter.once('move', handleOnce)
+
+emitter.removeEventListener('move', handleOnce)
 
 emitter.emit('move')
 
 emitter.emit('move')
 
-emitter.emit('move')
+// emitter.emit('move')
